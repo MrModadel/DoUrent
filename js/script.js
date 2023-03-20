d6a191a7525a97d5be5e737d78a3619a0f35f6e4
MrModadel <timanormu@gmail.com>
2023-03-20 20:24:51 +0500

    news

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 let items = document.querySelectorAll('.item')
-
 items.forEach(item => {
    item.onclick = () => {
       item.classList.toggle('active')
    }
 })
+
+$(document).ready(function(){
+   $('.slider').slick({
+      arrows:false,
+      dots:true,
+      infinite:false
+   });
+});
